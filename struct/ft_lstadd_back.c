117536ce7269cfae82e3292a43327b2937218e28
root <root@LAPTOP-VUMK21AS.localdomain>
2024-09-13 19:19:36 +0800

    struct

--- struct/ft_lstadd_back.c
+++ struct/ft_lstadd_back.c
@@ -4,25 +4,32 @@ void    ft_lstadd_back(t_list **lst, t_list *new)
 {
     t_list *last;
 
-    last = *lst;
-    if (last && new)
+    if (!lst || !new)
+        return ;
+    if (!*lst)
     {
-        while (last->next)
-            last = last->next;
-        last->next = new;
+        *lst = new;
+        return ;
     }
+    last = *lst;
+    while (last->next)
+        last = last->next;
+    last->next = new;
 }
 
+/*
+#include "string.h"
 int main()
 {
-    t_list *node1 = ft_lstnew("omg");
-    t_list *node2 = ft_lstnew("is");
-    t_list *node3 = ft_lstnew("xxxxx");
+    t_list *node1 = NULL;
+    t_list *node2 = ft_lstnew(strdup("is"));
+    t_list *node3 = ft_lstnew(strdup("xxxxx"));
 
     ft_lstadd_back(&node1, node2);
     ft_lstadd_back(&node1, node3);
 
-    printf("%s,%s,%s\n", (char *)node1->content, (char *)node1->next->content, (char *)node1->next->next->content);
+    printf("%s,%s\n", (char *)node1->content, (char *)node1->next->content);
   
     return 0;
 }
+*/
--- struct/ft_lstdelone.c
+++ struct/ft_lstdelone.c
@@ -4,27 +4,29 @@ void    ft_lstdelone(t_list *lst, void (*del)(void *))
 {
     if (lst && del)
     {
-        del(lst->content)
+        del(lst->content);
         free(lst);
     } 
 }
 
 /*
-void del(t_list *node)
+#include <string.h>
+void del(void *content)
 {
-    free(node->content);
+    free(content);
 }
 
 int main()
 {
-    t_list *node1 = ft_lstnew("omg");
+    t_list *node1 = ft_lstnew(strdup("omg"));
     
     printf("%s\n", (char *)node1->content);
 
     ft_lstdelone(node1, del);
     if (!node1)
         printf("nothing");
-
+    else
+        printf("ehhh");
     return 0;
 }
 */
