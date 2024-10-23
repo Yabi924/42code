02efd5e5ab1fe7ad1514d11cb9c45ecd9abe542b
Yong Yan Bin <yyan-bin@u90z03s08.42kl.edu.my>
2024-10-23 17:52:26 +0800

    push_swap makefile

---
+++ push_swap/src/main.c
@@ -0,0 +1,8 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    ft_printf("hi %s\n", argv[1]);
+    return 0;
+}
---
+++ push_swap/src/push_swap.h
@@ -0,0 +1,7 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../lib/libft/libft.h"
+# include "../lib/ft_printf/ft_printf.h"
+
+#endif
